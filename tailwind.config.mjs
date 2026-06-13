782051f7be3fb87308a91682cd330f52ad954d9d
anig <alexisnahuelidoyaga@gmail.com>
2026-06-13 15:49:45 -0300

    inicial

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {
+            screens: {
+                xs: '480px',
+                tablet: '768px',
+                laptop: '1024px',
+                desktop: '1440px',
+            },
+        },
+    },
+};
